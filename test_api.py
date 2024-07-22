fde41a7447736e5d2a2876c5c2514382ac534e33
Khatchik-Michel <105145707+Khatchik-Michel@users.noreply.github.com>
2024-07-22 07:10:14 +0200

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -4,28 +4,33 @@ import pandas as pd
 import json
 import requests
 
+
 class TestFlaskAPI(unittest.TestCase):
+    api_ip = 'http://192.168.99.89:5001'  #deuxieme IP si executer pas sur la même machine
 
     def test_change_model_endpoint_400(self):
         model_id = None
         # Create data for the POST request
         data = {"model_id": model_id}
-        response = requests.post('http://localhost:8501/new_model', data=json.dumps(data), headers={'Content-Type': 'application/json'})
+        response = requests.post(f'{self.api_ip}/new_model', data=json.dumps(data), headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status_code, 400)
 
     def test_predict_endpoint_with_valid_data(self):
         X = pd.read_csv("./test/X_head", index_col=0)
         data = X.to_json(orient='records')
-        response = requests.post('http://localhost:8501/predict', data=data)
+        response = requests.post(f'{self.api_ip}/predict', data=data, headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status_code, 200)
 
     def test_get_dataset_version(self):
-        response = requests.get('http://localhost:8501/version')
+        response = requests.get(f'{self.api_ip}/version')
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), "2.0")
 
     def test_get_dataset_threshold(self):
-        response = requests.get('http://localhost:8501/threshold')
+        response = requests.get(f'{self.api_ip}/threshold')
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), "0.1")
+
 
 if __name__ == '__main__':
     unittest.main()
