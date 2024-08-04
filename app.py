1e6d52089c495d92edbad02b052fe4b401324901
Khatchik-Michel <105145707+Khatchik-Michel@users.noreply.github.com>
2024-08-04 18:06:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,36 +13,49 @@ app = Flask(__name__)
 # Charger le modèle pré-entraîné
 model = joblib.load('model.pkl')
 
-def optimize_int_memory(df):
-    for col in df.select_dtypes(include=['int64', 'Int64']).columns:
-        max_val = df[col].max()
-        min_val = df[col].min()
-        if min_val >= 0:
-            if max_val <= 255:
-                df[col] = df[col].astype('uint8')
-            elif max_val <= 65535:
-                df[col] = df[col].astype('uint16')
-            elif max_val <= 4294967295:
-                df[col] = df[col].astype('uint32')
-        else:
-            if min_val >= -128 and max_val <= 127:
-                df[col] = df[col].astype('int8')
-            elif min_val >= -32768 and max_val <= 32767:
-                df[col] = df[col].astype('int16')
-            elif min_val >= -2147483648 and max_val <= 2147483647:
-                df[col] = df[col].astype('int32')
-    return df
+# Obtenir les noms des caractéristiques du modèle
+expected_features = model.feature_names_in_
+
+@app.route('/new_model', methods=['POST'])
+def new_model():
+    data = request.get_json(force=True)
+    model_id = data.get('model_id')
+    if model_id is None:
+        return jsonify({"error": "model_id is required"}), 400
+    # Logique pour changer de modèle
+    return jsonify({"message": "Model changed successfully"}), 200
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
-    df = pd.DataFrame(data)
-    
-    # Optimisation de la mémoire
-    df = optimize_int_memory(df)
-    
-    prediction = model.predict(df)
-    return jsonify(prediction.tolist())
+    try:
+        df = pd.DataFrame(data)  # Convertir les données en DataFrame
+
+        # Ajouter des colonnes manquantes avec une valeur par défaut appropriée
+        for feature in expected_features:
+            if feature not in df.columns:
+                df[feature] = 0  # Ou une autre valeur par défaut appropriée
+
+        # Réordonner les colonnes pour correspondre à l'ordre attendu par le modèle
+        df = df[expected_features]
+
+        # Convertir toutes les colonnes en types numériques
+        for col in df.columns:
+            df[col] = pd.to_numeric(df[col], errors='coerce').fillna(0)
+
+        prediction = model.predict_proba(df)
+        return jsonify(prediction.tolist())
+    except Exception as e:
+        print("Erreur lors de la prédiction:", e)  # Loguer les erreurs
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/version', methods=['GET'])
+def version():
+    return jsonify("2.0")
+
+@app.route('/threshold', methods=['GET'])
+def threshold():
+    return jsonify("0.1")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
